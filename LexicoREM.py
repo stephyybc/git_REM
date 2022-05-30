f75cf8791325a5fe5f5879feeac821e7360464a7
github_stephyybc <stephybenitez82@gmail.com>
2022-05-29 19:36:16 -0500

    Añadi el inicio del analisis de lexico

--- LexicoREM.py
+++ LexicoREM.py
@@ -3,7 +3,7 @@ import ply.yacc as yacc
 #import sys
 
 codigoPrueba = open('TEST.txt','r')
-
+#LEXICO
 #Con los tokens defino cada palabra de mi lenguaje
 tokens = [
     'INT',
@@ -32,10 +32,14 @@ tokens = [
     'OR', 
     'QUOM', #comillas 
     'STRING',
-    'ID',
+    'ID', #especificamente para funciones
+    'NOMBRE', #especificamente para variables y arreglos
     'ENDING', #semicolon
     'PLUSPLUS',
-    'DOTSEQ' #dos puntos igual (asignacion de valor)
+    'DOTSEQ', #dos puntos igual (asignacion de valor)
+    'STRING_VALUE', #el valor de string
+    'INT_VALUE', #el valor dentro del int
+    'FLOAT_VALUE' #el valor del float
 ]
 
 #Palabras reservadas de mi lenguaje
@@ -95,15 +99,39 @@ def t_COMMENT(t):
        r'\//.*' 
        pass
 
-def t_STRING(t):
+#palabra declarada string
+def t_STRING_VALUE(t):
      r'"([^"\n]|(\\"))*"' #Puede ser cualquiera de esos caracteres entre comillas
      return t
 
-def t_FLOAT(t): 
-    r'\d+\.\d+'
-    t.value = float(t.value)
+def t_INT_VALUE(t):
+    r'\d+' #recibe numero decimales
+    t.value = int(t.value) #accede al value y lo convierte a entero
     return t
-    
+
+def t_FLOAT_VALUE(t):
+     r'\d+\.\d+'
+     t.value = float(t.value)
+     return t
+
+#valor de string
+def t_STRING(t):
+     r'STRING'
+     t.type = 'STRING'
+     return t
+
+#valor de int
+def t_INT(t):
+    r'INT'
+    t.type = 'INT'
+    return t
+
+#valor de float
+def t_FLOAT(t):
+    r'FLOAT'
+    t.type = 'FLOAT'
+    return t
+
 #Palabra que inicia la funcion principal
 def t_STARTO(t): 
     r'STARTO'
@@ -116,10 +144,6 @@ def t_END(t):
     t.type = "END"
     return t
 
-def t_INT(t):
-    r'\d+' #recibe numero decimales
-    t.value = int(t.value) #accede al value y lo convierte a entero
-    return t
 
 def t_ID(t):
     r'[a-zA-Z][a-zA-Z_0-9]*'
@@ -128,6 +152,15 @@ def t_ID(t):
         t.type = t.value.upper()
     else:
             t.type = 'ID'
+    return t
+
+def t_NOMBRE(t):
+    r'[a-zA-Z][a-zA-Z_0-9]*'
+    if t.value.upper() in reserved: #revisar que está en el arreglo
+        t.value = t.value.upper()
+        t.type = t.value.upper()
+    else:
+            t.type = 'NOMBRE'
     return t
 
 def t_error(t):
@@ -142,3 +175,66 @@ while True:
     print(tokenss)
     if not tokenss: 
         break
+
+#Análsisi de sintaxis
+#DIAGRAMA DE SINTAXIS
+#precedence nos ayuda a jerarquizar las operaciones con mayor prioridad
+#entre más abajo mayor prioridad
+precedence = (
+    ('nonassoc', 'GTTHAN', 'LESSTHAN', 'GTEQ', 'LOEQ'), 
+    ('left','AND','OR'),
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'MULTIPLY', 'DIVIDE'),
+    #('right', 'UMINUS'),            # Unary minus operator
+)
+
+#ahora analizamos las funciones
+
+#Primero la funcion principal, estructura del programa
+def p_main(p):
+    '''
+    main : ID STARTO PARLEFT PARRIGHT DOTS body ID END
+    '''
+
+#vamos a declarar body
+#jerarquia a la izquierda
+def p_body(p):
+    '''
+    body : body create_var 
+         | empty
+    '''
+def p_createVar(p):
+    '''
+    create_var : ID DOTS type DOTSEQ value ENDING
+    '''
+
+def p_type(p):
+    '''
+    type : INT 
+         | FLOAT
+         | STRING     
+    '''
+
+def p_value(p):
+    '''
+    value : INT_VALUE 
+          | FLOAT_VALUE
+          | STRING_VALUE 
+    '''
+
+def p_empty(p):
+    'empty : '
+    pass
+
+def p_error(p):
+    print("ERROR FOUND!!")
+    print(p)
+
+sintaxis = yacc.yacc()
+try:
+    with open("TEST.txt",  encoding="utf8") as f:
+        file = f.read()
+    sintaxis.parse(file)
+except EOFError:
+    pass
+
